eca78d81f34f50d36bbd1e87446974d21ad967db
Charan Reddy <surecharanreddy@gmail.com>
2023-12-10 00:16:10 +0530

    updated hard problems

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,55 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor(){
+    this.result = 0;
+  }
+  add(num){
+    this.result += num
+  }
+
+  subtract(num){
+    this.result -= num
+  }
+
+  multiply(num){
+    this.result *= num
+  }
+
+  divide(num){
+    if(num===0){
+      throw new Error('Error')
+    }
+    else
+    this.result /= num
+  }
+
+  calculate(str){
+    str = str.split(' ').join('');
+    try{
+      let res = eval(str);
+      if( isNaN(res) || !isFinite(res)){
+        throw new Error('Error');
+      }
+      else 
+      return (this.result = res);
+
+    }
+    catch(error){
+      throw new Error('Error');
+    }
+  }
+
+  clear(){
+    this.result = 0;
+  }
+
+  getResult(){
+    return this.result;
+  }
+
+  
+}
 
 module.exports = Calculator;
