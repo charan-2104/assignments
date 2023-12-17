e582d89b340042ade19f52e23003fdbcbff5f81a
Charan Reddy <surecharanreddy@gmail.com>
2023-12-17 11:47:37 +0530

    solved todoServer

--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -45,5 +45,57 @@
   const app = express();
   
   app.use(bodyParser.json());
+
+  let todos = []
   
+  app.get('/todos/', function(req,res){
+    res.json(todos)
+  })
+
+  app.get("/todos/:id",function(req,res){
+    const todo = todos.find(t => t.id === parseInt(req.params.id))
+    if(!todo){
+    return res.status(404).send()
+    }
+    else
+    res.json(todo)
+  })
+
+  app.post("/todos/",function(req,res){
+    const newTodo = {
+      id: Math.floor(Math.random() * 1000000),
+      title: req.body.title,
+      description: req.body.description
+    }
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+  })
+
+  app.put("/todos/:id",function(req,res){
+    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id))
+    if(todoIndex == -1){
+      return res.status(404).send()
+    }
+    else{
+     todos[todoIndex].title = req.params.title
+     todos[todoIndex].description = req.params.description
+     res.json(todos[todoIndex])
+    }
+  })
+
+  app.delete("/todos/:id/",(req,res)=>{
+    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id))
+    if(todoIndex == -1){
+      res.status(404).send();
+    }
+    else{
+      todos.splice(todoIndex,1)
+      res.status(200).send();
+    }
+  })
+
+  app.use((req,res,next) => {
+    res.status(404).send()
+  })
+
   module.exports = app;
